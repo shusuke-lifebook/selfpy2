1a150fe0f976dd3a69c6fdb7a789dd40de77e0ee
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-01-01 07:52:03 +0900

    リスト5.9まで学習完了
    5.2.3 部分文字列を取得するの記載を追加

---
+++ chap05/str_lower.py
@@ -0,0 +1,11 @@
+data1 = "Wings Project"
+data2 = "self learn python"
+data3 = "Fußball"
+
+print(data1.lower)  # Wings Project
+print(data1.upper())  # WINGS PROJECT
+print(data1.swapcase())  # wINGS pROJECT
+print(data2.capitalize())  # Self learn python
+print(data2.title())  # Self Learn Python
+print(data3.lower())  # fußball
+print(data3.casefold())  # fussball
