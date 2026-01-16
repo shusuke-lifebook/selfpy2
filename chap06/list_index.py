98bb664d420e3cfce1f0a39311a82f41324210a5
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-01-17 08:23:24 +0900

    リスト6.21 役職をもとにソートするまで学習完了

---
+++ chap06/list_index.py
@@ -0,0 +1,5 @@
+data = ["い", "ろ", "は", "に", "ほ", "へ", "と", "い", "ろ"]
+print(data.index("い"))
+print(data.index("い", 4))
+# print(data.index("い", 4, 7)) # エラー
+print(data.index("い", -4, -1))
---
+++ chap06/list_remove2.py
@@ -0,0 +1,3 @@
+data = ["鈴木", "田中", "井上", "加藤", "田中", "河野"]
+data = [elem for elem in data if elem != "田中"]
+print(data)
