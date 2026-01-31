d6c86837725498244c2bfd8abe630412160f8c6f
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-01-31 09:12:10 +0900

    8.1 ユーザー定義関数の基本

---
+++ chap08/func_basic.py
@@ -0,0 +1,8 @@
+# get_triangle関数を定義
+def get_triangle(base, height):
+    return base * height / 2
+
+
+# get_triangle関数を呼び出す
+area = get_triangle(8, 10)
+print(f"三角形の面積は{area}です。")
---
+++ chap08/practice/p_func.py
@@ -0,0 +1,6 @@
+def get_diamond(diamond1, diamond2):
+    return diamond1 * diamond2
+
+
+area = get_diamond(8, 10)
+print(f"菱形の面積は{area}です。")
